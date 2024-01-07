43425826f4e6ce94bd07cb2f07338afb81b370b2
robertotonde <robertotonde316@gmail.com>
2024-01-07 15:18:04 +0300

    search bar start

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { SearchContext } from "./context/searchContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchContext>
+      <App />
+    </SearchContext>
   </React.StrictMode>
 );
